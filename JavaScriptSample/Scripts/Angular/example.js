4908dc8ad9d270307ae3084b21422b575b19283a
Chris Murphy <cmurph1624@gmail.com>
2015-11-04 18:40:13 -0700

    Button push causes alerts

---
+++ JavaScriptSample/Scripts/Angular/example.js
@@ -0,0 +1,24 @@
+﻿angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('AlertDemoCtrl', function ($scope) {
+    $scope.alerts = [];
+    $scope.statusModel;
+    $scope.addAlert = function () {
+        $scope.alerts.push({ type: 'info', msg: 'Another alert!' });
+    };
+
+    $scope.$watch('statusModel', function () {
+        if ($scope.statusModel === "warning") {
+            $scope.alerts.push({ type: $scope.statusModel, msg: 'Warning Alert!' });
+        } else if ($scope.statusModel === "danger") {
+            $scope.alerts.push({ type: $scope.statusModel, msg: 'Danger Alert!' });
+        } else if ($scope.statusModel === "info") {
+            $scope.alerts.push({ type: $scope.statusModel, msg: 'Info alert!' });
+        }
+
+
+    });
+
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
+});
---
+++ JavaScriptSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaScriptSample.Startup))]
+namespace JavaScriptSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
